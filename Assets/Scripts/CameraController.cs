e3857e9a0a7f958d41c87e6f70d4a418cc44a250
przemyslaw-buczek <przemyslaw.buczek95@gmail.com>
2025-04-12 09:50:44 -0400

    Dodano PlayerController i CameraController

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] float mouseSensitivity = 100f;
+    Transform playerBody;
+
+    float xRotation = 0f;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        playerBody = transform.parent;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CameraRotation();
+    }
+
+    void CameraRotation()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90, 80);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float speed = 12;
+    CharacterController characterController;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMove();
+    }
+
+    void PlayerMove()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        characterController.Move(move * speed * Time.deltaTime);
+    }
+}
