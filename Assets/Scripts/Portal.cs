a39bccc0752d5876a859e007a514f0ad7ef66ff5
przemyslaw-buczek <przemyslaw.buczek95@gmail.com>
2025-05-17 10:52:36 -0400

    Po lekcji portal 1

---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    [SerializeField] Portal otherPortal;
+    [SerializeField] Material material;
+
+    public Camera myCamera;
+
+    public Transform renderSurface;
+    public Transform portalCollider;
+
+    private GameObject player;
+    private PortalTeleport portalTeleport;
+    private PortalCamera portalCamera;
+
+    private void Awake()
+    {
+        player = GameObject.FindWithTag("Player");
+        portalTeleport = portalCollider.GetComponent<PortalTeleport>();
+        portalTeleport.player = player.transform;
+        portalTeleport.receiver = otherPortal.portalCollider;
+
+        portalCamera = myCamera.GetComponent<PortalCamera>();
+        portalCamera.playerCamera = player.GetComponentInChildren<Camera>().transform;
+        portalCamera.otherPortal = otherPortal.transform;
+        portalCamera.portal = transform;
+        
+        renderSurface.GetComponent<Renderer>().material = Instantiate(material);
+        
+        if (myCamera.targetTexture != null)
+        {
+            myCamera.targetTexture.Release();
+        }
+
+        myCamera.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+    }
+
+    private void Start()
+    {
+        renderSurface.GetComponent<Renderer>().material.mainTexture =
+            otherPortal.GetComponent<Portal>().myCamera.targetTexture;
+    }
+}
---
+++ Assets/Scripts/PortalTeleport.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalTeleport : MonoBehaviour
+{
+    public Transform player;
+    public Transform receiver;
+    
+    private bool playerIsOverlapping = false;
+
+    private void FixedUpdate()
+    {
+        Teleportaion();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerIsOverlapping = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerIsOverlapping = false;
+        }
+    }
+
+    private void Teleportaion()
+    {
+        if (playerIsOverlapping)
+        {
+            Vector3 portalToPlayer = player.position - transform.position;
+            float dotProduct = Vector3.Dot(transform.up, portalToPlayer);
+
+            if (dotProduct < 0)
+            {
+                float rotationDiff = -Quaternion.Angle(transform.rotation, receiver.rotation);
+                rotationDiff += 180;
+
+                Vector3 positionOffset = Quaternion.Euler(0f, rotationDiff, 0f) * portalToPlayer;
+
+                player.position = receiver.position + positionOffset;
+
+                playerIsOverlapping = false;
+            }
+        }
+    }
+}
