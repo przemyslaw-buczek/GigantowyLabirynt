f99d9761f1f5569ba9bae722c54c8e8b49c9fd06
przemyslaw-buczek <przemyslaw.buczek95@gmail.com>
2025-05-31 10:02:43 -0400

    Dodalem klucz

--- Assets/Scripts/PortalCamera.cs
+++ Assets/Scripts/PortalCamera.cs
@@ -10,7 +10,26 @@ public class PortalCamera : MonoBehaviour
 
     void Update()
     {
+        PositionCamera();
+    }
 
+    void PositionCamera()
+    {
+        Vector3 playerOffsetFromPortal = playerCamera.position - otherPortal.position;
+        transform.position = portal.position + playerOffsetFromPortal;
+
+        float angularDifferenceBetweenPortalRotations =
+            Quaternion.Angle(portal.rotation, otherPortal.rotation);
+
+        Quaternion portalRotationalDifference =
+            Quaternion.AngleAxis(angularDifferenceBetweenPortalRotations, Vector3.up);
+
+        Vector3 newCameraDirection = portalRotationalDifference * playerCamera.forward;
+
+        newCameraDirection =
+            new Vector3(newCameraDirection.x * -1, newCameraDirection.y, newCameraDirection.z * -1);
+
+        transform.rotation = Quaternion.LookRotation(newCameraDirection, Vector3.up);
     }
 
 }
